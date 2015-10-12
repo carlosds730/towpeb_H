5e4ce74a7a8dc9cf1a260bd7e6b739ced771bef6
Carlos <carlos_ds730@yahoo.es>
2015-10-12 15:08:36 -0400

    Making payments

--- Shop_Site/views.py
+++ Shop_Site/views.py
@@ -38,7 +38,14 @@ def get_login(cookies):
 
 def home(request):
     if request.method == 'GET':
-        return add_info_home(request, {})
+        try:
+            request.COOKIES['successful_shop']
+            ret = add_info_home(request, {'successful_purchase': True})
+            ret.delete_cookie("successful_shop")
+            return ret
+        except KeyError:
+            print("yuju")
+            return add_info_home(request, {})
 
 
 def add_info_home(request, data, url='base.html'):
@@ -919,18 +926,39 @@ def payment_methods(request):
     else:
         on_hold = get_purchase(request.COOKIES)
 
-    print(str(on_hold.total_price_two()[1]))
+    try:
+        result = braintree.Transaction.sale({
+            "amount": on_hold.total_price_two()[1],
+            "payment_method_nonce": nonce,
+            "options": {
+                "submit_for_settlement": True
+            }
+        })
+    except:
+        print("It sucks")
+        result = None
+        pass
 
-    result = braintree.Transaction.sale({
-        "amount": on_hold.total_price_two()[1],
-        "payment_method_nonce": nonce,
-        "options": {
-            "submit_for_settlement": True
-        }
-    })
-    print(result.is_success)
-    # True
+    succes_id = None
+    error_message = None
 
-    print(result.transaction.type)
+    # TODO: succes_id should be saved in database.
+    # TODO: Deleting things from the database and send the proper emails.
+    if result:
+        ret = HttpResponseRedirect('/')
+        ret.set_cookie('successful_shop', True)
+        succes_id = format(result.transaction.id)
+        return ret
 
-    print(result.transaction.status)
+    else:
+        try:
+            error_message = format(result.message)
+        except:
+            error_message = "I can´t go online"
+        try:
+            token = braintree.ClientToken.generate()
+        except Exception:
+            token = 'eyJ2ZXJzaW9uIjoyLCJhdXRob3JpemF0aW9uRmluZ2VycHJpbnQiOiJmMDU0MTRmZTc1MGNiYmIwOGQ0YTM1MGIwZmYyYmMxMWE5ZDc0ZWMzNmMxM2QwYjkzNjExYWNiMTUyYTRmNjhhfGNyZWF0ZWRfYXQ9MjAxNS0wOS0wNFQyMDo0NTozMS4zMjE0ODU4MzErMDAwMFx1MDAyNm1lcmNoYW50X2lkPTM0OHBrOWNnZjNiZ3l3MmJcdTAwMjZwdWJsaWNfa2V5PTJuMjQ3ZHY4OWJxOXZtcHIiLCJjb25maWdVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMzQ4cGs5Y2dmM2JneXcyYi9jbGllbnRfYXBpL3YxL2NvbmZpZ3VyYXRpb24iLCJjaGFsbGVuZ2VzIjpbXSwiZW52aXJvbm1lbnQiOiJzYW5kYm94IiwiY2xpZW50QXBpVXJsIjoiaHR0cHM6Ly9hcGkuc2FuZGJveC5icmFpbnRyZWVnYXRld2F5LmNvbTo0NDMvbWVyY2hhbnRzLzM0OHBrOWNnZjNiZ3l3MmIvY2xpZW50X2FwaSIsImFzc2V0c1VybCI6Imh0dHBzOi8vYXNzZXRzLmJyYWludHJlZWdhdGV3YXkuY29tIiwiYXV0aFVybCI6Imh0dHBzOi8vYXV0aC52ZW5tby5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIiwiYW5hbHl0aWNzIjp7InVybCI6Imh0dHBzOi8vY2xpZW50LWFuYWx5dGljcy5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tIn0sInRocmVlRFNlY3VyZUVuYWJsZWQiOnRydWUsInRocmVlRFNlY3VyZSI6eyJsb29rdXBVcmwiOiJodHRwczovL2FwaS5zYW5kYm94LmJyYWludHJlZWdhdGV3YXkuY29tOjQ0My9tZXJjaGFudHMvMzQ4cGs5Y2dmM2JneXcyYi90aHJlZV9kX3NlY3VyZS9sb29rdXAifSwicGF5cGFsRW5hYmxlZCI6dHJ1ZSwicGF5cGFsIjp7ImRpc3BsYXlOYW1lIjoiQWNtZSBXaWRnZXRzLCBMdGQuIChTYW5kYm94KSIsImNsaWVudElkIjpudWxsLCJwcml2YWN5VXJsIjoiaHR0cDovL2V4YW1wbGUuY29tL3BwIiwidXNlckFncmVlbWVudFVybCI6Imh0dHA6Ly9leGFtcGxlLmNvbS90b3MiLCJiYXNlVXJsIjoiaHR0cHM6Ly9hc3NldHMuYnJhaW50cmVlZ2F0ZXdheS5jb20iLCJhc3NldHNVcmwiOiJodHRwczovL2NoZWNrb3V0LnBheXBhbC5jb20iLCJkaXJlY3RCYXNlVXJsIjpudWxsLCJhbGxvd0h0dHAiOnRydWUsImVudmlyb25tZW50Tm9OZXR3b3JrIjp0cnVlLCJlbnZpcm9ubWVudCI6Im9mZmxpbmUiLCJ1bnZldHRlZE1lcmNoYW50IjpmYWxzZSwiYnJhaW50cmVlQ2xpZW50SWQiOiJtYXN0ZXJjbGllbnQzIiwiYmlsbGluZ0FncmVlbWVudHNFbmFibGVkIjpmYWxzZSwibWVyY2hhbnRBY2NvdW50SWQiOiJhY21ld2lkZ2V0c2x0ZHNhbmRib3giLCJjdXJyZW5jeUlzb0NvZGUiOiJVU0QifSwiY29pbmJhc2VFbmFibGVkIjpmYWxzZSwibWVyY2hhbnRJZCI6IjM0OHBrOWNnZjNiZ3l3MmIiLCJ2ZW5tbyI6Im9mZiJ9'
+        return add_info_home(request, {'on_hold': on_hold, 'token': token, 'error': error_message},
+                             'info_card.html')
+        # DONE: If result.is_success this should go somewhere. Is not result.is_success then when should go back and say
