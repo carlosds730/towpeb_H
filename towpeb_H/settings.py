2e4a5a5b1f5254dcb3940da665f008534ac00a97
Carlos <carlos_ds730@yahoo.es>
2016-01-01 13:57:36 -0500

    Discount percent changed

--- towpeb_H/settings.py
+++ towpeb_H/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'f-!cid_mmvn(8f+7wz6^vh#qwx3-zf303i-y)%kb3pkb9r(%be'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURE_SSL_REDIRECT = True
 
@@ -72,24 +72,24 @@ BRAINTREE_MERCHANT = '5mbk28tz7tp7vzsp'
 BRAINTREE_PUBLIC_KEY = 'xbzyp9nzm24gk5zg'
 BRAINTREE_PRIVATE_KEY = '4febe8b21f7144c0186b366031d55556'
 
-# braintree.Configuration.configure(
-#     braintree.Environment.Production,
-#     '5mbk28tz7tp7vzsp',
-#     'xbzyp9nzm24gk5zg',
-#     '4febe8b21f7144c0186b366031d55556'
-# )
+braintree.Configuration.configure(
+    braintree.Environment.Production,
+    '5mbk28tz7tp7vzsp',
+    'xbzyp9nzm24gk5zg',
+    '4febe8b21f7144c0186b366031d55556'
+)
 
 # BRAINTREE_ENV = braintree.Environment.Sandbox
 # BRAINTREE_MERCHANT = 'rgrr3976xdtj9r9x'
 # BRAINTREE_PUBLIC_KEY = 'wp9wm59tpzg5ht43'
 # BRAINTREE_PRIVATE_KEY = '223d029cad5b5aa6dbcb93b3fba280a0'
 #
-braintree.Configuration.configure(
-    braintree.Environment.Sandbox,
-    'rgrr3976xdtj9r9x',
-    'wp9wm59tpzg5ht43',
-    '223d029cad5b5aa6dbcb93b3fba280a0'
-)
+# braintree.Configuration.configure(
+#     braintree.Environment.Sandbox,
+#     'rgrr3976xdtj9r9x',
+#     'wp9wm59tpzg5ht43',
+#     '223d029cad5b5aa6dbcb93b3fba280a0'
+# )
 
 # If you cannot install M2Crypto (e.g. AppEngine):
 # BRAINTREE_UNSAFE_SSL = True
